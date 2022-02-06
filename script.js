cfb20b939c309edb0ac8ba65b69e02dc42173bed
JUNAisKIND <0627juna@naver.com>
2022-02-06 17:57:20 +0900

    add tip

--- script.js
+++ script.js
@@ -8,19 +8,14 @@ toggleBtn.addEventListener('click', () => {
   nav.classList.toggle('active');
   menu.classList.toggle('active');
   sns.classList.toggle('active');
-})
-
+}) // #navbar_toogle 버튼을 눌렀을때 ative class toggle
 class afterText extends HTMLElement {
   connectedCallback() {
     const text = this.getAttribute("text") ? this.getAttribute("text") : "";
-
     this.attachShadow({mode : 'open'});
-    this.shadowRoot.innerHTML = `
-    <span></span>
-    <style>span::after {content: "${text ? text : ""}";}</style>
-    `
-
-
+    this.shadowRoot.innerHTML = `<span></span><style>span::after {content: "${text ? text : ""}";}</style>`
   }
 }
 customElements.define('af-t', afterText);
+// 텍스트 복사 방지를 위해 :after 로 텍스트를 추가하는 custom element
+// <af-t text="Text"> 로 사용
